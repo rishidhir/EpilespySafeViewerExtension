19d72f6db0e745b78f2ff7a5380bbf276035ffda
suyashhaspowers <suyashhaspowers@gmail.com>
2021-08-21 18:00:08 -0400

    extension fix

--- content.js
+++ content.js
@@ -5,23 +5,23 @@ BLOCK = {
 }
 
 
-
-function webpageNavigate() {
+function afterNavigate() {
     if ('/watch' === location.pathname) {
         
         var url = new URL(location.href);
-        getShieldTimesForVideo(location.href)
-            .then((shieldTimes) => {
-                resetShieldTimes(shieldTimes)
+        getBlockingTimesForVideo(location.href)
+            .then((blockingTimes) => {
+                resetBlockingTimes(blockingTimes)
             })
             .catch((error) => {
-                console.error('Error: No blocking times:', error);
+                console.error('Error:', error);
             })
     }
 }
 
-function getShieldTimesForVideo(){
-    return fetch('https://localhost:5000/get_blocked_timestamps?url='+location.href, {
+
+function getBlockingTimesForVideo(){
+    return fetch('http://127.0.0.1:5000/get_potential_seizure_timestamps?url='+location.href, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -30,81 +30,90 @@ function getShieldTimesForVideo(){
         })
         .then(response => response.json())
         .then(data => {
-            console.log('Success: we have gotten data:', data);
+            console.log('Success:', data);
             return data['result']
         })
 }
 
 
-
-function resetShieldTimes(newShieldTimes){
+function resetBlockingTimes(newBlockingTimes){
     BLOCK = {
-        times: newShieldTimes,
+        times: newBlockingTimes,
         state: 0
     }
 }
 
-function setFilter(update){
-    var cover = document.getElementsByClassName('ESV');
-    var videos = document.getElementsBy("video");
-    if(cover.length == 0){
-        var youtube_video = document.getElementsByTagName("video");
-        // if(youtube_video.length ==0){
-        //     return
-        // }
 
-        shield = document.createElement("div")
-        shield.innerHTML = 'Epilepsy Safety Shield Active'
-        shield.className = 'ESV'
-        shield.style.display = 'none'
-        // shield.style.position = "relative";
-        // shield.style.width = youtube_video.style.width;
-        // shield.style.height = youtube_video.style.height;
-        shield.style.backgroundColor = "black";
-        shield.style.textAlign = "center";
-        shield.style.opacity = "0.9";
-        shield.style.color = "lightgray";
-        shield.style.fontSize = "50px";
-        // shield.style.paddingTop = "200px";
-        // youtube_video[0].parentElement.appendChild(cover)
-        cover = document.getElementsByClassName('ESV')        
-
+function setBlockingFilter(update){
+    var masks = document.getElementsByClassName('seizure-control')
+    var videos = document.getElementsByTagName("video");
+    if (masks.length == 0){
+        var videos = document.getElementsByTagName("video");
+        if (videos.length == 0){
+            return
+        }
+        mask = document.createElement('div')
+        mask.innerHTML = "Seizure-Savior Activated!"
+        mask.className = 'seizure-control';
+        mask.style.display = "none"
+        mask.style.position = "relative";
+        mask.style.width = "853px";
+        mask.style.height = "480px";
+        mask.style.backgroundColor = "black";
+        mask.style.left = "409px";
+        mask.style.top = "0px";
+        mask.style.textAlign = "center";
+        mask.style.opacity = "0.9";
+        mask.style.color = "lightgray";
+        mask.style.fontSize = "50px";
+        mask.style.paddingTop = "200px";
+        videos[0].parentElement.appendChild(mask)
+        masks = document.getElementsByClassName('seizure-control')
     }
 
-    shield=cover[0]
-    youtube_video = videos[0]
-
-    if(update){
-
-        shield.style.display = "block"
-        shield.style.width = youtube_video.style.width;
-        shield.style.height = youtube_video.style.height;
-        shield.style.left = youtube_video.style.left;
-        shield.style.top = youtube_video.style.top;
+    mask = masks[0]
+    video = videos[0]
+    if (update){
+        mask.style.display = "block"
+        mask.style.width = video.style.width;
+        mask.style.height = video.style.height;
+        mask.style.left = video.style.left;
+        mask.style.top = video.style.top;
         
     } else {
-        shield.style.display = "none"
+        mask.style.display = "none"
     }
     
+    
 
+    // var videos = document.getElementsByTagName("video");
+    // if (videos.length == 0){
+    //     return
+    // }
+
+
+    // if (update){
+    //     videos[0].style.filter = "brightness(50%) blur(25px) grayscale(100%)";
+    // } else {
+    //     videos[0].style.filter = "brightness(100%) blur(0px) grayscale(0%)";
+    // }
 }
+// https://www.youtube.com/watch?v=uxKPVD5KJCY
 
 
-function activateShield(){
-
-   streams = document.getElementsByClassName('video-stream')
-   
-   stream = streams[0];
-
-   current_time = stream.currentTime;
-
-   while (BLOCK['state'] < BLOCK['times'].length && currtime >= BLOCK['times'][BLOCK['state']][0]){
-    setShieldFilter(BLOCK['times'][BLOCK['state']][1])
-    BLOCK['state'] += 1
+function checkFilter(){
+    video_streams = document.getElementsByClassName('video-stream')
+    if (video_streams.length == 0){
+        return
     }
-
-
+    video_stream = video_streams[0]
+    currtime = video_stream.currentTime;
+    while (BLOCK['state'] < BLOCK['times'].length && currtime >= BLOCK['times'][BLOCK['state']][0]){
+        setBlockingFilter(BLOCK['times'][BLOCK['state']][1])
+        BLOCK['state'] += 1
+    }
 }
+
 
 setInterval(() => {
     if (CURRENT_PAGE != location.href){
@@ -113,10 +122,8 @@ setInterval(() => {
     }
 }, 1000)
 
-
 setInterval(() => {
-    activateShield()
+    checkFilter()
 }, 50)
 
-
-webpageNavigate();
+afterNavigate();
